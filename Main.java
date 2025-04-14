730b4274149f07f44102be352ebe39ac76be73a9
ron-ct <ronkaranja@outlook.com>
2025-04-14 16:38:43 +0300

    fixed some minor bugs, code is okay

--- Main.java
+++ Main.java
@@ -13,8 +13,10 @@ public class Main {
         System.out.println("Door B: floors 6 - 8");
         System.out.println("Door C: floors 9 - 10");
         System.out.println("Type 'exit' to quit\n");
-        
+
+
         while (true) {
+            System.out.println();
             System.out.println("Enter requested floor (0 - 10): ");
             System.out.println("0 for Ground floor\n1 - 1st\n2 - 2nd\n3 - 3rd\n4 - 4th\n5 - 5th\n6 - 6th\n7 - 7th\n8 - 8th\n9 - 9th\n10 - 10th\n: ");
             String input = scanner.nextLine();
@@ -38,7 +40,22 @@ public class Main {
             
             // If valid floor is entered, move the elevator.
             elevator.moveToFloor(num);
-            break;  // Exit the loop after a successful elevator move.
+            System.out.println("Elevator will be ready to serve you in 5");
+            System.out.println();
+
+            try{
+                for(int i = 5; i > 0; i--){
+                    System.out.print("\r" + i);
+                    Thread.sleep(1000);
+                }
+                continue;
+
+            }catch(InterruptedException e){
+                e.printStackTrace();
+
+            }
+            System.out.println();
+            continue;
         }
         
         scanner.close();
